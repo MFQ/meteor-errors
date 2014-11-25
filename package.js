800a53fc1559b03dc87a6c240742ab7b610f1c71
MFQ <softmfq@gmail.com>
2014-11-25 18:16:38 +0500

    mfq:errors.js is added in package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mfq:errors',
   summary: 'A pattern to display errors to users',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/MFQ/meteor-errors.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   // api.addFiles('mfq:errors.js');
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
-  api.addFiles(['errors_list.js', 'errors_list.html', 'errors_list.js'], 'client');
+  api.addFiles(['errors_list.js', 'errors_list.html', 'mfq:errors.js'], 'client');
   if (api.export)
     api.export('Errors');
 
