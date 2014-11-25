d5e3f7d3b0b53d792dec933ba4228e5994fda8e6
MFQ <softmfq@gmail.com>
2014-11-25 18:54:55 +0500

    errors collection name is changed from 'errors' to null

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mfq:errors',
   summary: 'A pattern to display errors to users',
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/MFQ/meteor-errors.git'
 });
 
