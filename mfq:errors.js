8315f8a3a4bbe38f5699fff6f9367f4d8e94ff0c
MFQ <softmfq@gmail.com>
2014-11-25 18:22:53 +0500

    hash arguments is added to Errors collection

--- mfq:errors.js
+++ mfq:errors.js
@@ -2,6 +2,6 @@ Errors = {
   // Local (client-only ) collection
   collection: new Mongo.collection("errors"),
   throw: function(message){
-    Errors.collection.insert(message: message, seen:false);
+    Errors.collection.insert({message: message, seen:false});
   }
 }
