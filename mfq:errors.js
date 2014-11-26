cd1fc5f1aeb2dd2485a22e7a0117b5b92f29a0b8
MFQ <softmfq@gmail.com>
2014-11-26 15:59:27 +0500

    Publisher, subscriber and errors collection is added

--- mfq:errors.js
+++ mfq:errors.js
@@ -1,7 +1,17 @@
 Errors = {
   // Local (client-only ) collection
-  collection: new Mongo.Collection(null),
+  collection: new Mongo.Collection("errors"),
   throw: function(message){
     Errors.collection.insert({message: message, seen:false});
   }
+}
+
+if (Meteor.isServer) {
+    Meteor.publish('errors', function(){
+      return Errors.collection.find();
+    });
+}
+
+if(Meteor.isClient){
+  Meteor.subscribe('errors');
 }
