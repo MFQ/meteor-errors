5735bee15bccc75bcd8d53df4b38b6d87ebcac0f
MFQ <softmfq@gmail.com>
2014-11-26 16:33:09 +0500

    Package is updated

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mfq:errors',
   summary: 'A pattern to display errors to users',
-  version: '1.0.6',
+  version: '1.0.7',
   git: 'https://github.com/MFQ/meteor-errors.git'
 });
 
